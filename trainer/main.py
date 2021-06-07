cd8a1823fbe184faf8e7871560b8c3a53455fd09
Youngsoo Lee <prevdev@gmail.com>
2021-06-07 22:10:46 +0900

    Update training code to use argparse

--- trainer/main.py
+++ trainer/main.py
@@ -1,11 +1,39 @@
+import argparse
+import os
+import time
+from datetime import datetime
 import torch
 import torchvision
-import os
-from datetime import datetime
+import torchvision.transforms as transforms
 
 from model import ResNet18
 from utils import AverageMeter, accuracy, replace_labels
 
+
+os.environ['TZ'] = 'Asia/Seoul'
+time.tzset()
+
+parser = argparse.ArgumentParser(description='Training with Clustered ImageNet Labels')
+parser.add_argument('data', metavar='DIR', help='path to dataset')
+parser.add_argument('--original-labels', default=False, type=bool, metavar='O',
+                    help='use original labels instead of cil')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('--epochs', default=40, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('-b', '--batch-size', default=256, type=int,
+                    metavar='N', help='mini-batch size (default: 256)')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--lr-step', default=10, type=int,
+                    metavar='N', help='step size of step scheduler')
+parser.add_argument('--lr-step-gamma', default=0.5, type=float,
+                    metavar='N', help='gamma value of step scheduler')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)',
+                    dest='weight_decay')
 
 def train(epoch, trainloader, model, criterion, optimizer, replacing_labels=False):
     losses = AverageMeter('Loss', ':.5f')
@@ -35,7 +63,7 @@ def train(epoch, trainloader, model, criterion, optimizer, replacing_labels=Fals
         loss.backward()
         optimizer.step()
 
-        if i % 100 == 0:
+        if i % 200 == 0:
             timestamp = datetime.now().replace(microsecond=0).isoformat().replace('T', ' ')
             print(f'{timestamp} [{epoch}][{i}/{len(trainloader)}] {losses} {top1} {top5}')
 
@@ -67,50 +95,69 @@ def validate(testloader, model, replacing_labels=False):
     return top1.avg, top5.avg
 
 
-def main(replacing_labels=False):
-    transform = torchvision.transforms.Compose([
-        torchvision.transforms.Resize((32, 32)),
-        torchvision.transforms.ToTensor(),
-    ])
-
-    datapath = '/home/ubuntu/dataset/imagenet_64_64_images/'
-
-    trainset = torchvision.datasets.ImageFolder(datapath + 'train', transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=1024, shuffle=True, num_workers=16)
-    testset = torchvision.datasets.ImageFolder(datapath + 'val', transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=4)
-
+def main(args):
+    replacing_labels = not args.original_labels
     num_classes = 488 if replacing_labels else 1000
+
+    # Make a dir for recording experimental result
+    start_time = datetime.now().replace(microsecond=0).isoformat().replace('T', ' ')
+    experiment_result_dir = os.path.join('result', start_time)
+    os.makedirs(experiment_result_dir, exist_ok=True)
+
+    # Create a log file
+    with open(os.path.join(experiment_result_dir, 'log.log'), 'a') as fout:
+        for key, value in vars(args).items():
+            fout.write(f'- {key}: {value}\n')
+        fout.write('---------------------------')
+
+    # Dataset
+    traindir = os.path.join(args.data, 'train')
+    valdir = os.path.join(args.data, 'val')
+    normalize = transforms.Normalize(mean=[0.4810, 0.4574, 0.4078],
+                                     std=[0.2146, 0.2104, 0.2138])
+
+    trainset = torchvision.datasets.ImageFolder(traindir, transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize,
+    ]))
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
+
+    testset = torchvision.datasets.ImageFolder(valdir, transforms.Compose([
+        transforms.ToTensor(),
+        normalize,
+    ]))
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers)
+
+    # Model
     model = ResNet18(num_classes)
     model.cuda()
 
+    # Loss, optimizer, scheduler
     criterion = torch.nn.CrossEntropyLoss().cuda()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.05, momentum=0.9, weight_decay=1e-4)
-    epochs = 30
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_step, gamma=args.lr_step_gamma)
 
-    for epoch in range(1, 1 + epochs):
+    # Training
+    for epoch in range(1, 1 + args.epochs):
         print(f'Epoch #{epoch}')
 
         train_loss, train_acc1, train_acc5 = train(epoch, trainloader, model, criterion,
                                                    optimizer, replacing_labels)
         test_acc1, test_acc5 = validate(testloader, model, replacing_labels)
 
-        os.makedirs('checkpoints', exist_ok=True)
-        torch.save({
-            'epoch': epoch,
-            'state_dict': model.state_dict(),
-            'avg_loss': train_loss,
-            'train_acc1': train_acc1,
-            'train_acc5': train_acc5,
-            'test_acc1': test_acc1,
-            'test_acc5': test_acc5,
-        }, f'checkpoints/resnet18_imagenet-{num_classes}_{epoch}.pth')
+        scheduler.step()
 
-        with open(f'training_log.log', 'a') as fout:
+        os.makedirs(os.path.join(experiment_result_dir, 'checkpoints'), exist_ok=True)
+        torch.save(model.state_dict(), os.path.join(experiment_result_dir, 'checkpoints', f'{epoch}.pth'))
+
+        with open(os.path.join(experiment_result_dir, 'log.log'), 'a') as fout:
             timestamp = datetime.now().replace(microsecond=0).isoformat().replace('T', ' ')
             fout.write(f'{timestamp}\tEpoch {epoch}\tLoss {train_loss:.4f}\tTrain acc@1 {train_acc1:.2f}'
                        f'\tTrain acc@5 {train_acc5:.2f}\tTest acc@1 {test_acc1:.2f}\tTest acc@5 {test_acc5:.2f}\n')
 
-
 if __name__ == '__main__':
-    main()
+    args = parser.parse_args()
+    main(args)
